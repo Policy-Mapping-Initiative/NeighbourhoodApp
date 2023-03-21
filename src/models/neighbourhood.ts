2b32df59c85fb8c3a9284c1a4f894b4000d8c03d
Rocky Petkov <rocky@weeklybeat.org>
2023-03-20 23:30:20 -0400

    Initial type work for NeighbourhoodApp

---
+++ src/models/neighbourhood.ts
@@ -0,0 +1,119 @@
+import { 
+    MultiPolygon,
+    FeatureCollection,
+    Feature,
+    GeoJsonProperties 
+} from 'geojson';
+
+import {Zone} from './zone'
+
+interface Props {
+    name: string,
+    neighbourhoodId: number,
+    area: number,
+    population: number,
+    density: number,
+    residenceCount: number,
+    singleFamilyCount: number,
+    midHighRiseCount: number,
+    otherAttachedCount: number,
+    semiDetachedCount: number,
+    rowHouseCount: number,
+    duplexCount: number,
+    lowRiseCount: number,
+    zones: Array<string>,
+    residentialArea: number,
+    mixedUseArea: number,
+    nonResidentialArea: number,
+    openSpaceArea: number,
+    singleFamilyArea: number
+}
+
+export class Neighbourhood implements Feature {
+
+    type: 'Feature';
+    properties: Props;
+    geometry: MultiPolygon;
+
+    constructor(feature: any) {
+        this.type = 'Feature'
+        this.properties = feature.properties
+        this.geometry = feature.geometry
+    }
+
+    get name() {
+        return this.properties.name;
+    }
+
+    get neighbourhoodId() {
+        return this.properties.neighbourhoodId;
+    }
+
+    get area() {
+        return this.properties.area;
+    }
+
+    get population() {
+        return this.properties.area;
+    }
+
+    get density() {
+        return this.properties.density;
+    }
+
+    get residenceCount() {
+        return this.properties.residenceCount;
+    }
+
+    get singleFamilyCount() {
+        return this.properties.singleFamilyCount;
+    }
+
+    get midHighRiseCount() {
+        return this.properties.midHighRiseCount;
+    }
+
+    get otherAttachedCount() {
+        return this.properties.otherAttachedCount;
+    }
+
+    get semiDetachedCount() {
+        return this.properties.semiDetachedCount;
+    }
+
+    get rowHouseCount() {
+        return this.properties.rowHouseCount;
+    }
+
+    get duplexCount() {
+        return this.properties.duplexCount;
+    }
+
+    get lowRiseCount() {
+        return this.properties.lowRiseCount;
+    }
+
+    get zones() {
+        return this.properties.zones;
+    }
+
+    get residentialArea() {
+        return this.properties.residentialArea;
+    }
+
+    get nonResidentialArea() {
+        return this.properties.nonResidentialArea;
+    }
+
+    get mixedUseArea() {
+        return this.properties.mixedUseArea;
+    }
+
+    get openSpaceArea() {
+        return this.properties.openSpaceArea;
+    }
+
+    get singleFamilyArea() {
+        return this.properties.singleFamilyArea;
+    }
+}
---
+++ src/models/zone.ts
@@ -0,0 +1,56 @@
+import { 
+    MultiPolygon,
+    FeatureCollection,
+    Feature,
+    GeoJsonProperties 
+} from 'geojson';
+import { LandUse, ZoneType } from './enums';
+import { Neighbourhood } from './neighbourhood';
+
+interface Props {
+    neighbourhoodId: number,
+    area: number,
+    zoneType: ZoneType,
+    landUse: LandUse,
+    id: string
+}
+
+export class Zone implements Feature {
+    type: 'Feature';
+    properties: Props;
+    geometry: MultiPolygon;
+    // neighbourhood: Neighbourhood;
+    imputedPopulation: Map<Number, Number>;
+
+    constructor(feature: any) {
+        
+        this.properties = feature.properties;
+        this.geometry = feature.geometry;
+        this.type = "Feature";
+
+
+        // TODO: Figure out where to store neighbourhoods stored by id so we can fetch a reference.
+        // this.neighbourhood = null; 
+        this.imputedPopulation = new Map(); 
+
+        // TODO: Make this more evergreen
+        // this.imputedPopulation.set(2023, (this.neighbourhood.density * this.area ))
+
+    }
+
+    get area() { 
+        return this.properties.area;
+    }
+
+    get zoneType() { 
+        return this.properties.zoneType;
+    }
+
+    get landUse() { 
+        return this.properties.landUse;
+    }
+
+    get id() { 
+        return this.properties.id;
+    }
+}
