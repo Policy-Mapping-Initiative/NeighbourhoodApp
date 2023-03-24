a3f9cc2dae6e0b571798bbc8456c486f106fc355
Sami <samirodrigue@hotmail.com>
2023-03-23 22:23:57 -0400

    add button with modal popup (#1)
    * add button with modal popup
    
    * centralize styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { Map } from './components/map';
 import { TopAppBar } from './components/appBar';
 import { styled } from '@mui/material/styles';
-import { useElementSize, useLocalStorage } from 'usehooks-ts';
+import { useLocalStorage } from 'usehooks-ts';
 import { useEffect } from 'react';
 
 const Main = styled('div')({});
 
 export function App() {
-  const [containerRef, { width, height }] = useElementSize(); // eslint-disable-line @typescript-eslint/no-unused-vars
   const [searchText, setSearchText] = useLocalStorage('searchText', '');
   const [zone, setZone] = useLocalStorage('zone', 'single');
 
@@ -22,7 +21,7 @@ export function App() {
   return (
     <Main>
       <TopAppBar setSearchBar={setSearchText} />
-      <Map parentSizeRef={containerRef} setZone={setZone} zoneVar={zone} />
+      <Map setZone={setZone} zoneVar={zone} />
     </Main>
   );
 }
