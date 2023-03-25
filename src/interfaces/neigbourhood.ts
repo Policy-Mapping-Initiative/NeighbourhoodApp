ca827e73da732e519f23cbdebe4b8f8bac01ae77
sarodz <samirodrigue@hotmail.com>
2023-03-25 16:16:46 -0400

    add interface

---
+++ src/interfaces/neigbourhood.ts
@@ -0,0 +1,44 @@
+import { MultiPolygon } from 'geojson';
+
+export interface INeighbourhood {
+  type: string;
+  crs: CRS;
+  features?: NeighbourhoodFeature[] | null;
+}
+
+export interface CRS {
+  type: string;
+  properties: CRSProps;
+}
+
+interface CRSProps {
+  name: string;
+}
+
+export interface NeighbourhoodFeature {
+  type: string;
+  properties: NeigbourhoodProps;
+  geometry: MultiPolygon;
+}
+
+export interface NeigbourhoodProps {
+  id: number;
+  name: string;
+  population: number;
+  area: number;
+  residenceCount: number;
+  singleFamilyCount: number;
+  midHighRiseCount: number;
+  otherAttachedCount: number;
+  semiDetachedCount: number;
+  rowHouseCount: number;
+  duplexCount: number;
+  lowRiseCount: number;
+  density: number;
+  zones?: string[] | null;
+  residentialArea: number;
+  nonResidentialArea: number;
+  mixedUseArea: number;
+  openSpaceArea: number;
+  singleFamilyArea: number;
+}
