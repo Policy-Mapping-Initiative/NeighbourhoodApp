42c24cb39986d1280d60b3de96edf36d6902e8c4
Sami <samirodrigue@hotmail.com>
2023-05-09 16:57:46 -0400

    remove hamburger (#29)

--- src/components/appBar/menu.tsx
+++ src/components/appBar/menu.tsx
@@ -1,28 +1,15 @@
 import * as React from 'react';
-import MenuIcon from '@mui/icons-material/Menu';
+import LocationCityIcon from '@mui/icons-material/LocationCity';
 import IconButton from '@mui/material/IconButton';
-import { Menu, MenuItem } from '@mui/material';
 import { useAppDispatch } from '../../store';
 import { openPolicyModal } from '../../reducers/policySlice';
+import { Typography } from '@mui/material';
 
 export default function BasicMenu() {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
   const dispatch = useAppDispatch();
-
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
 
   const handleMenuClick = () => {
     dispatch(openPolicyModal());
-    handleClose();
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-
-    // Disaptch Action to open the our modal
   };
 
   return (
@@ -31,26 +18,12 @@ export default function BasicMenu() {
         size="large"
         edge="start"
         color="inherit"
-        aria-label="open drawer"
         sx={{ mr: 2 }}
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onClick={handleClick}
+        onClick={handleMenuClick}
       >
-        <MenuIcon />
+        <LocationCityIcon />
+        <Typography>Policy</Typography>
       </IconButton>
-      <Menu
-        id="top-left-menu"
-        open={open}
-        onClose={handleClose}
-        anchorEl={anchorEl}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-      >
-        <MenuItem onClick={handleMenuClick}>City Wide Policies</MenuItem>
-      </Menu>
     </div>
   );
 }
